0b898520d2573521b34f372eb6e9d8e02684fffe
OldBigBuddha <shinlag@outlook.jp>
2016-03-05 20:16:29 +0900

    やばし（；^ω^）

---
+++ app/src/main/java/com/intechschool/oji/test_list_db/MainActivity.java
@@ -0,0 +1,81 @@
+package com.intechschool.oji.test_list_db;
+
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.activeandroid.query.Select;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    EditText createToDo;
+    Button btAdd;
+    ToDoDB mDB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView)findViewById(R.id.listView);
+        createToDo = (EditText)findViewById(R.id.editToDo);
+        btAdd = (Button)findViewById(R.id.btCreate);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ListView list = (ListView)parent;
+                String selectedItem = (String) list.getItemAtPosition(position);
+                Log.d("DEBUG","selected" + selectedItem);
+
+            }
+        });
+
+        mDB = new ToDoDB();
+
+       setToDoList();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        setToDoList();
+    }
+
+    public void onAdd(View v) {
+        saveToDo();
+        createToDo.setText("");
+    }
+
+    public void saveToDo() {
+        mDB.todo = createToDo.getText().toString();
+        mDB.save();
+        mDB = new ToDoDB();
+        setToDoList();
+
+
+    }
+
+    void setToDoList() {
+        List<ToDoDB> todoList = new Select().from(ToDoDB.class).execute();
+        ArrayAdapter<ToDoDB> adapter = new ArrayAdapter<>(
+                getApplicationContext(),
+                R.layout.todo_row,
+                todoList
+        );
+
+        listView.setAdapter(adapter);
+    }
+}
+
---
+++ app/src/main/java/com/intechschool/oji/test_list_db/ToDoDB.java
@@ -0,0 +1,29 @@
+package com.intechschool.oji.test_list_db;
+
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+
+/**
+ * Created by 1 on 16/03/05.
+ */
+@Table(name = "todo_table")
+public class ToDoDB extends Model {
+
+    @Column(name = "todo")
+    public String todo;
+
+    @Override
+    public String toString() {
+        return todo;
+    }
+
+    public ToDoDB() {
+        super();
+    }
+
+    public ToDoDB(String name) {
+        super();
+        this.todo = todo;
+    }
+}
