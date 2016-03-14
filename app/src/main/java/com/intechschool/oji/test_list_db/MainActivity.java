5f0a10d01d058811b8b4e5ef837c36bde29892a6
OldBigBuddha <shinlag@outlook.jp>
2016-03-14 17:45:32 +0900

    アイコン追加、ちょいと

--- app/src/main/java/com/intechschool/oji/test_list_db/MainActivity.java
+++ app/src/main/java/com/intechschool/oji/test_list_db/MainActivity.java
@@ -1,12 +1,18 @@
 package com.intechschool.oji.test_list_db;
 
 import android.app.Activity;
+import android.app.AlarmManager;
 import android.app.AlertDialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteCursor;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.widget.ExploreByTouchHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -23,6 +29,7 @@ import com.activeandroid.query.Select;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
@@ -37,6 +44,9 @@ public class MainActivity extends Activity {
     ToDoDB mDB;
     ToDoDB selectedItem;
     boolean is_Edit;
+    Notification notification;
+    NotificationManager notificationManager;
+    NotificationCompat.Builder mBuilder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -96,13 +106,14 @@ public class MainActivity extends Activity {
                 selectedItem.delete();
                 setToDoList();
 
-                return  null;
-                }
+                return null;
+            }
 
         });
         listView.enableSwipeToDismiss();
 
         mDB = new ToDoDB();
+        notification = new Notification();
 
         setToDoList();
 
@@ -147,13 +158,39 @@ public class MainActivity extends Activity {
     }
 
     public void saveToDo(int priority) {
+        //変数宣言
         ToDoDB saveDB = new ToDoDB();
-        saveDB.todo = createToDo.getText().toString();
+        String save_Str =createToDo.getText().toString();
+        saveDB.todo = save_Str;
         saveDB.priority = priority;
+
+        /* notificationManager */
+        mBuilder = new NotificationCompat.Builder(this);
+        mBuilder.setSmallIcon(R.drawable.icon);
+        mBuilder.setContentTitle(save_Str);
+        mBuilder.setContentText(save_Str + "が追加されました");
+        notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0,mBuilder.build());
+        /**/
+
+//        Intent i = new Intent(getApplicationContext(),MainActivity.class);
+//        PendingIntent sender = PendingIntent.getBroadcast(this, 0, i, 0);
+//
+//        Calendar calendar = Calendar.getInstance();
+//        calendar.setTimeInMillis(System.currentTimeMillis());
+//        calendar.add(Calendar.SECOND, 10);
+//
+//        AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
+//        alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),sender);
+
+
+
+        //日付設定
         Date date = new Date();
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.JAPANESE);
         saveDB.date = sdf.format(date);
         saveDB.save();
+
         setToDoList();
 
     }
